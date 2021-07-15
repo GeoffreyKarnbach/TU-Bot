4ff92c75c0fe22791dd28f56c83c9a578dd6cc8f
DerGoyote <36735387+GeoffreyKarnbach@users.noreply.github.com>
2021-07-15 20:46:36 +0200

    Base bot

---
+++ cogs/global_.py
@@ -0,0 +1,63 @@
+import discord
+from discord.ext import commands
+import sqlite3
+from .Modules.paginator import*
+
+class Global(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+        self.db = sqlite3.connect("Data/database.db")
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Bot is ready")
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        pass
+
+    @commands.command()
+    async def ping(self,ctx):
+        await ctx.send("Pong!")
+        
+    @commands.command()
+    async def prefix(self, ctx, *, prefix = "."):
+        with open('Configuration/server_setting.json', "r") as json_file:
+            data = json.load(json_file)
+            data["prefix"] = prefix
+        
+        with open('Configuration/server_setting.json', "w") as json_file:
+            json_file.write(json.dumps(data))
+
+        await ctx.send(f"New prefix will be {prefix}")
+        
+
+    @commands.command(aliases=['s'])
+    async def shutdown(self,ctx):
+        if ctx.message.author.id == 450259740153479189:
+            await ctx.send("Bot shuting down.")
+            exit(-1)
+        else:
+            await ctx.send("Missing permissions.")
+    
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, event):
+        await paginator_edit(event, self.client)
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        await ctx.send("An error occured when using this command.")
+        await ctx.send("```"+str(error)+"```")
+
+
+            
+def setup(client):
+    client.add_cog(Global(client))
+
+
+
+
+    
+    
+
---
+++ main.py
@@ -0,0 +1,50 @@
+from discord.ext import commands, tasks
+import discord
+import os
+import sqlite3
+from cogs.Modules.sql_utils import*
+from cogs.Modules.json import*
+import asyncio
+from datetime import datetime
+import json
+
+###################################################################### COG UNLOAD + LOAD    ################################################
+def load_all():
+    for file in os.listdir("cogs"):
+        if file.endswith(".py"):
+            print(f"===== {file[:-3]} loaded =====")
+            client.load_extension("cogs."+file[:-3])
+
+def unload_all():
+    for file in os.listdir("cogs"):
+        if file.endswith(".py"):
+            print(f"===== {file[:-3]} unloaded =====")
+            client.unload_extension("cogs."+file[:-3])
+
+
+###################################################################### DATABASE MANAGING    ################################################
+database = sqlite3.connect("Data/database.db")
+create_table(database, "paginator", "(page integer, pageNB integer, messageId integer, embeds blob)")
+reset_table(database, "paginator")
+######################################################################     DISCORD BOT    ################################################
+
+async def get_pre(bot, message):
+    with open('Configuration/server_setting.json') as json_file:
+        data = json.load(json_file)
+        prefix = data["prefix"]
+    return prefix
+
+client = commands.Bot(command_prefix = get_pre)
+
+@client.command()
+async def reload_cogs(ctx):
+    unload_all()
+    load_all()
+    await ctx.send("All cogs have been reloaded.")
+
+    
+# Load all cogs
+load_all()
+
+######################################################################   RUN BOT WITH TOKEN    ################################################
+client.run(readJSONFile('Configuration', 'token.json')['token'])
